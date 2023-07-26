71b7c9e8195c30de4a03bee9951f6dd14f28e8c6
git-rbk <140586366+git-rbk@users.noreply.github.com>
2023-07-26 12:18:15 +0530

    Update streamlit_app.py
    Added breakfast menu and its options

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents new healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Idli and Sambar')
+streamlit.text('Poha')
+streamlit.text('Medu wada')
+streamlit.text('Upma')
+streamlit.text('Ghavanaa')
